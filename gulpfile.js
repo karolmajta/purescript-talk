a229082d45c97220c119a0c81d9e7d47c56f509d
Karol Majta <karolmajta@gmail.com>
2015-04-29 00:47:30 +0200

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var purescript = require('gulp-purescript');
+
+gulp.task('purescript:browser', function () {
+  return gulp.src('src/**/*.purs')
+    .pipe(purescript.psc({main: 'Talk', output: 'app.js'}))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('purescript:node', function () {
+  return gulp.src('src/**/*.purs')
+    .pipe(purescript.pscMake({output: 'purescript_modules'}))
+});
+
+gulp.task('purescript:psci', function () {
+  return gulp.src(['src/**/*.purs', '!src/**/Talk.purs'])
+    .pipe(purescript.dotPsci());
+});
+
+gulp.task('default', ['purescript:browser', 'purescript:node', 'purescript:psci']);
